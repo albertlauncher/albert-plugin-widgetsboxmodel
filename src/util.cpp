5133d8b9fec09cb11dc3c235f29e83d0199c2630
Manuel Schneider <manuelschneid3r@gmail.com>
2026-02-18 11:17:31 +0100

    feat: Use stylehints to detect dark mode

--- src/util.cpp
+++ src/util.cpp
@@ -1,15 +1,23 @@
 // Copyright (c) 2023-2025 Manuel Schneider
 
 #include "util.h"
-#include <QWidget>
 #include <QApplication>
 #include <QStyle>
+#include <QStyleHints>
+#include <QWidget>
 
 bool haveDarkSystemPalette()
 {
-    auto pal = QApplication::palette();
-    return pal.color(QPalette::WindowText).lightness()
-           > pal.color(QPalette::Window).lightness();
+    if (const auto sh = qApp->styleHints()->colorScheme();
+        sh == Qt::ColorScheme::Dark)
+        return true;
+    else if (sh == Qt::ColorScheme::Light)
+        return false;
+    else {
+        auto pal = QApplication::palette();
+        return pal.color(QPalette::WindowText).lightness()
+               > pal.color(QPalette::Window).lightness();
+    }
 }
 
 void setStyleRecursive(QWidget *widget, QStyle *style)
