9647910c7ba8e7347c808148c56b527c260d0892
Manuel Schneider <manuelschneid3r@gmail.com>
2026-03-31 12:01:11 +0200

    fix: Revert "Cache icon by item identifier"
    This reverts commit cdc7a9a67123150a19bde5a79c9ce12b092f1de5.
    
    Caching by item id effectively breaks the dymamic item feature. Data changed
    signals triggers a repaint, but, since the item id is still the same, the old
    cached icon is picked. Reverting back to the icon id fixes this.
    
    cdc7a9a tries to allow empty icon ids and as such avoids them entirely which
    also has a relaxing effect on the icon construction cost requirements (had to be
    fast). This approach is a dead end though since we need the icon id as described
    above.
    
    So from now on:
    - Every icon has to have cache key
    - Icon construction has to be fast,
      (Because we may instantiate icons just to get cache keys a lot)

--- src/resultslist.cpp
+++ src/resultslist.cpp
@@ -146,26 +146,21 @@ void ResultsListDelegate::paint(QPainter *p,
                                                          subtext_rect.width());
 
     QPixmap pm;
-    if (const auto cache_key = u"%1@%2x%3"_s
-                                   .arg(i.data(IdentifierRole).toString())
-                                   .arg(icon_size)
-                                   .arg(o.widget->devicePixelRatioF());
-        !QPixmapCache::find(cache_key, &pm))
+    if (const auto icon = i.data(IconRole).value<QIcon>();
+        icon.isNull())
+        WARN << "Item retured null icon:" << i.data(IdentifierRole).value<QString>();
+    else if (const auto icon_name = icon.name();
+        icon_name.isEmpty())
+        WARN << "Item has no icon name set. Refusing to use icon without a cache key:"
+             << i.data(IdentifierRole).value<QString>();
+    else if (const auto cache_key = u"%1@%2x%3"_s.arg(icon_name).arg(icon_size).arg(o.widget->devicePixelRatioF());
+            !QPixmapCache::find(cache_key, &pm))
     {
         auto tp = system_clock::now();
-        if (const auto icon = i.data(IconRole).value<QIcon>();
-            icon.isNull())
-            WARN << "Item retured null icon:"
-                 << i.data(IdentifierRole).value<QString>();
-        else
-        {
-            pm = icon.pixmap(QSize(icon_size, icon_size), o.widget->devicePixelRatioF());
-            auto dur = duration_cast<milliseconds>(system_clock::now() - tp).count();
-            if (dur > 5)
-                WARN << u"Slow icon rendering: %1 ms - %2"_s.arg(dur).arg(icon.name());
-        }
-
-
+        pm = icon.pixmap(QSize(icon_size, icon_size), o.widget->devicePixelRatioF());
+        auto dur = duration_cast<milliseconds>(system_clock::now() - tp).count();
+        if (dur > 5)
+            WARN << u"Slow icon rendering: %1 ms - %2"_s.arg(dur).arg(icon.name());
         QPixmapCache::insert(cache_key, pm);
     }
 
