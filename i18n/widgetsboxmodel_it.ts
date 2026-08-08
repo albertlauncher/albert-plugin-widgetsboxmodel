d0a608037daa7524c4997391e1906e59fba2929e
Francesco Ferrari <62111448+flakyferr@users.noreply.github.com>
2026-08-08 16:21:43 +0200

    feat: Add Italian (it) translation

---
+++ i18n/widgetsboxmodel_it.ts
@@ -0,0 +1,243 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>ConfigWidget</name>
+    <message>
+        <source>Frontend settings</source>
+        <translation>Impostazioni interfaccia</translation>
+    </message>
+    <message>
+        <source>Enable history search</source>
+        <translation>Attiva cronologia di ricerca</translation>
+    </message>
+    <message>
+        <source>Use the current input text to filter the input history.</source>
+        <translation>Usa l&apos;input corrente per filtrare la cronologia di input.</translation>
+    </message>
+    <message>
+        <source>Clear input line on hide</source>
+        <translation>Nascondere Albert azzera la riga di input</translation>
+    </message>
+    <message>
+        <source>Clear the input line when the window is hidden.</source>
+        <translation>Cancella il testo in input quando la finestra viene nascosta.</translation>
+    </message>
+    <message>
+        <source>Always on top</source>
+        <translation>Sempre in cima</translation>
+    </message>
+    <message>
+        <source>Let Albert stay on top of all top level windows.</source>
+        <translation>Mantieni Albert in cima a tutti i livelli di finestre.</translation>
+    </message>
+    <message>
+        <source>Hide on focus out</source>
+        <translation>Nascondi quando il focus è perso</translation>
+    </message>
+    <message>
+        <source>Hide Albert if the window lost focus.</source>
+        <translation>Nascondi Albert se il focus sulla finestra viene perso.</translation>
+    </message>
+    <message>
+        <source>Show centered</source>
+        <translation>Mostra centrato</translation>
+    </message>
+    <message>
+        <source>Position the window centered on the screen. See also &quot;Center on active screen&quot;.</source>
+        <translation>Posiziona la finestra al centro dello schermo. Vedi anche &quot;Centra sullo schermo attivo&quot;.</translation>
+    </message>
+    <message>
+        <source>Center on active screen</source>
+        <translation>Centra sullo schermo attivo</translation>
+    </message>
+    <message>
+        <source>Show the window on the active screen (the one containing the mouse cursor) rather than the primary one. This option applies when &quot;Show centered&quot; is activated, but also if the window has to be centered on another screen (e.g. because a display has been disconnected).</source>
+        <translation>Mostra la finestra sullo schermo attivo (quello che contiene il puntatore del mouse) invece che sullo schermo primario. Questa opzione si applica quando &quot;Mostra centrato&quot; è attivo, ma anche se la finestra dev&apos;essere centrata su un altro schermo (ad esempio perché un display è stato scollegato).</translation>
+    </message>
+    <message>
+        <source>Light theme</source>
+        <translation>Tema chiaro</translation>
+    </message>
+    <message>
+        <source>Set the color theme used in light mode.</source>
+        <translation>Imposta il tema colori utilizzato in modalità chiara.</translation>
+    </message>
+    <message>
+        <source>Displayed items</source>
+        <translation>Elementi mostrati</translation>
+    </message>
+    <message>
+        <source>Display scrollbar</source>
+        <translation>Mostra barra di scorrimento</translation>
+    </message>
+    <message>
+        <source>Displays a scrollbar in the results list.</source>
+        <translation>Mostra una barra di scorrimento nella lista dei risultati.</translation>
+    </message>
+    <message>
+        <source>Set the color theme used in dark mode.</source>
+        <translation>Imposta il tema colori usato in modalità scura.</translation>
+    </message>
+    <message>
+        <source>Dark theme</source>
+        <translation>Tema scuro</translation>
+    </message>
+    <message>
+        <source>Debug mode</source>
+        <translation>Modalità debug</translation>
+    </message>
+    <message>
+        <source>Disable input method</source>
+        <translation>Disattiva metodo di input</translation>
+    </message>
+    <message>
+        <source>Window properties</source>
+        <translation>Proprietà finestra</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Modifica</translation>
+    </message>
+    <message>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+    <message>
+        <source>Window</source>
+        <translation>Finestra</translation>
+    </message>
+    <message>
+        <source>Shadow size</source>
+        <translation>Dimensione ombra</translation>
+    </message>
+    <message>
+        <source>Shadow offset</source>
+        <translation>Offset ombra</translation>
+    </message>
+    <message>
+        <source>Width</source>
+        <translation>Larghezza</translation>
+    </message>
+    <message>
+        <source>Border radius</source>
+        <translation>Raggio bordi</translation>
+    </message>
+    <message>
+        <source>Border width</source>
+        <translation>Larghezza bordi</translation>
+    </message>
+    <message>
+        <source>Padding</source>
+        <translation>Riempimento</translation>
+    </message>
+    <message>
+        <source>Spacing</source>
+        <translation>Spaziatura</translation>
+    </message>
+    <message>
+        <source>Input</source>
+        <translation>Input</translation>
+    </message>
+    <message>
+        <source>Font size</source>
+        <translation>Dimensione carattere</translation>
+    </message>
+    <message>
+        <source>Results</source>
+        <translation>Risultati</translation>
+    </message>
+    <message>
+        <source>Description font size</source>
+        <translation>Dimensione carattere descrizione</translation>
+    </message>
+    <message>
+        <source>Selection border radius</source>
+        <translation>Raggio bordo di selezione</translation>
+    </message>
+    <message>
+        <source>Selection border width</source>
+        <translation>Larghezza bordo di selezione</translation>
+    </message>
+    <message>
+        <source>Icon size</source>
+        <translation>Dimensione icona</translation>
+    </message>
+    <message>
+        <source>Horizontal spacing</source>
+        <translation>Spaziatura orizzontale</translation>
+    </message>
+    <message>
+        <source>Vertical spacing</source>
+        <translation>Spaziatura verticale</translation>
+    </message>
+    <message>
+        <source>Actions</source>
+        <translation>Azioni</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Switch themes</source>
+        <translation>Cambia tema</translation>
+    </message>
+    <message>
+        <source>Use in light mode</source>
+        <translation>Usa in modalità chiara</translation>
+    </message>
+    <message>
+        <source>Use in dark mode</source>
+        <translation>Usa in modalità scura</translation>
+    </message>
+    <message>
+        <source>Open theme file</source>
+        <translation>Apri file del tema</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Hide on focus out</source>
+        <translation>Nascondi quando il focus è perso</translation>
+    </message>
+    <message>
+        <source>Show centered</source>
+        <translation>Mostra centrato</translation>
+    </message>
+    <message>
+        <source>Clear on hide</source>
+        <translation>Cancella quando nascosto</translation>
+    </message>
+    <message>
+        <source>Debug mode</source>
+        <translation>Modalità debug</translation>
+    </message>
+    <message>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+    <message>
+        <source>The system theme.</source>
+        <translation>Il tema di sistema.</translation>
+    </message>
+    <message>
+        <source>Input edit mode</source>
+        <translation>Modalità di modifica input</translation>
+    </message>
+    <message>
+        <source>Failed loading theme</source>
+        <translation>Errore durante il caricamento del tema</translation>
+    </message>
+    <message>
+        <source>Themes</source>
+        <translation>Temi</translation>
+    </message>
+    <message>
+        <source>themes</source>
+        <extracomment>The trigger</extracomment>
+        <translation>temi</translation>
+    </message>
+</context>
+</TS>
